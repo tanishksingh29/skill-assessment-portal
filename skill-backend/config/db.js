a15949a6291aed7f3484c10156a6c2a9a231491b
tanishksingh29 <146875427+tanishksingh29@users.noreply.github.com>
2025-08-28 15:12:17 +0530

    Update db.js

--- skill-backend/config/db.js
+++ skill-backend/config/db.js
@@ -1,12 +1,15 @@
 require('dotenv').config(); // only needed for local development
-const mysql = require('mysql2');
+const mysql = require('mysql2/promise');
 
-const connection = mysql.createConnection({
+const db = mysql.createPool({
   host: process.env.MYSQLHOST,
   port: process.env.MYSQLPORT,
   user: process.env.MYSQLUSER,
   password: process.env.MYSQLPASSWORD,
   database: process.env.MYSQLDATABASE,
+  waitForConnections: true,
+  connectionLimit: 10, // max 10 simultaneous connections
+  queueLimit: 0,
 });
 
 connection.connect((err) => {
