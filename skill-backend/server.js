006cb2d8215ddf1684962d93546b9364e8293fb5
tanishksingh29 <146875427+tanishksingh29@users.noreply.github.com>
2025-08-28 14:40:58 +0530

    Update server.js

--- skill-backend/server.js
+++ skill-backend/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config(); // local development
-const db = require('./db'); // import the db connection
+const db = require('./config/db'); // import the db connection
 
 const authRoutes = require('./routes/authRoutes');
 const skillRoutes = require('./routes/skillRoutes');
